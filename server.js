a18aed7fb5853f73b987a625827b78892362c688
Maria Luiza <altenhofen.loth@gmail.com>
2026-06-22 15:52:24 -0300

    feat: teste

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
     res.send('API do Catálogo de Produtos está rodando!');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
